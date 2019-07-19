01eeee94513bed865ba808ace504b7e56be614f1
Artur Laksberg <arturl@microsoft.com>
2019-07-19 00:19:47 -0700

    New APIs

--- bot/Bots/EchoBot.cs
+++ bot/Bots/EchoBot.cs
@@ -13,6 +13,25 @@ namespace Microsoft.BotBuilderSamples.Bots
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
+            if(turnContext.Activity.Text.Contains("human"))
+            {
+                var a1 = MessageFactory.Text($"msg1");
+                var a2 = MessageFactory.Text($"msg2");
+                var transcript = new Activity[] { a1, a2 };
+                var context = new { Skill = "credit cards ", CallerId = "CCI" };
+                IHandoffRequest request = await turnContext.InitiateHandoffAsync(transcript, context, cancellationToken);
+
+                if(await request.IsCompletedAsync())
+                {
+                    await turnContext.SendActivityAsync("Handoff request has been completed");
+                }
+                else
+                {
+                    await turnContext.SendActivityAsync("Handoff request has NOT been completed");
+                }
+
+                return;
+            }
             await turnContext.SendActivityAsync(MessageFactory.Text($"Echo: {turnContext.Activity.Text}"), cancellationToken);
         }
 
@@ -22,7 +41,7 @@ namespace Microsoft.BotBuilderSamples.Bots
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {
-                    await turnContext.SendActivityAsync(MessageFactory.Text($"Hello and welcome!"), cancellationToken);
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Hello and welcome5!"), cancellationToken);
                 }
             }
         }
